479a423a2456ee54fd1c1b077775702913b64abd
werik-antunes <127903405+werik-antunes@users.noreply.github.com>
2024-03-19 21:11:36 -0300

    Add files via upload

---
+++ Tarefa09Ex02.java
@@ -0,0 +1,17 @@
+package exercicios;
+
+public class Tarefa09Ex02 {
+
+	public static void main(String[] args) {
+
+/*2) Elaborar um programa que apresente no final o somatório dos valores pares existentes 
+* na faixa de 1 até 500.*/
+		
+		for(int num = 1; num < 501; num++) {
+			if(num % 2 == 0) {
+				System.out.println("A soma fina é: " + num);
+			}
+		}
+			}	
+	}
+
